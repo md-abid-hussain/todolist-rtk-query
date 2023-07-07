7e98ab78d8292be22e1bd9458abe32fac54eaff1
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-07-07 17:42:36 +0530

    created TodoList component

--- src/features/todo/TodoList.jsx
+++ src/features/todo/TodoList.jsx
@@ -1,18 +1,38 @@
+import {
+    useGetTodosQuery,
+    useAddTodoMutation,
+    useDeleteTodoMutation,
+    useUpdateTodoMutation
+} from '../api/apiSlice'
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faUpload } from "@fortawesome/free-solid-svg-icons"
+import { faUpload, faTrash } from "@fortawesome/free-solid-svg-icons"
 import { useState } from "react"
 
 const TodoList = () => {
     const [todo, setTodo] = useState('');
 
+    const {
+        data: todos,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetTodosQuery()
+
+    const [addTodo] = useAddTodoMutation();
+    const [updateTodo] = useUpdateTodoMutation();
+    const [deleteTodo] = useDeleteTodoMutation();
+
     const handleSubmit = (e) => {
         e.preventDefault()
+        addTodo({ userId: 1, title: todo, completed: false })
         setTodo('')
     }
 
     const newItemSection =
-        <form onSubmit={handleSubmit} className="flex">
-            <label htmlFor="new-todo">Enter a new todo item</label>
+        <form onSubmit={handleSubmit} className="flex border">
+            <label className="outside" htmlFor="new-todo">Enter a new todo item</label>
             <div className="new-todo">
                 <input
                     type="text"
@@ -29,10 +49,36 @@ const TodoList = () => {
             </button>
         </form>
 
+    let content;
+    if (isLoading) {
+        content = <p>Loading...</p>
+    } else if (isSuccess) {
+        content = todos.map(todo => {
+            return (
+                <article key={todo.id} className="flex border mt-half">
+                    <div className="todo flex">
+                        <input type="checkbox"
+                            checked={todo.completed}
+                            id={todo.id}
+                            onChange={() => updateTodo({ ...todo, completed: !todo.completed })}
+                        />
+                        <label htmlFor={todo.id}>{todo.title}</label>
+                    </div>
+                    <button className='trash' onClick={() => deleteTodo({ id: todo.id })}>
+                        <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                </article>
+            )
+        })
+    } else if (isError) {
+        content = <p>{error.status} </p>
+    }
+
     return (
         <main>
             <h1>Todo List</h1>
             {newItemSection}
+            <div className="contents">{content}</div>
         </main>
     )
 }
